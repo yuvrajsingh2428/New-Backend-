c69b60cb7746b8e55a703ca49dc6fba614b3c5c9
yuvrajsingh2428 <offcyuvi2428@gmail.com>
2024-08-11 15:33:15 +0530

    Add getWatchHistory endpoint

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose, { mongo } from "mongoose";
 
 
 //5a
@@ -432,22 +433,73 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
                 isSubscribed: 1,
                 avatar: 1,
                 coverImage: 1,
-                email: email
+                email: 1
+            }
+        }     
+]) 
+
+        if(!channel?.length){
+            throw new ApiError(404, "Channel does not exist")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "User channel fetched successfully")
+    )
+}) 
+
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user  = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup:"users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as:"owner",
+                        pipline: [{
+                            $project: {
+                                $project: {
+                                    fullName: 1 ,
+                                    username: 1,
+                                    avatar:1
+                                }
+                            }
+                        }]
+
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                owner:{
+                    $first: "$owner "
+                }
             }
         }
-        
-    ]) 
-
-    if(!channel?.length){
-        throw new ApiError(404, "Channel does not exist")
-    }
-
+    ])
     return res
     .status(200)
     .json(
-        new ApiResponse(200, "User channel fetched successfully")
-    )
-}) 
+        new ApiResponse (
+            200, 
+            user[0].getWatchHistory,
+            "Watch history fetched successfully"
+        )
+    )            
+})
 
 
 export {
@@ -459,7 +511,9 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
 
 // to logout a user we made a middleware named auth.middleware.js 
